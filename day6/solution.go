303a31fa9cba45141b9307a527ad03c4f07cc467
Martijn Majoor <m.majoor@awl.nl>
2019-12-26 14:01:34 +0100

    Add solution for day six (part two)

--- day6/solution.go
+++ day6/solution.go
@@ -8,18 +8,10 @@ import (
 )
 
 func main() {
-	orbits := file.ReadLines("orbits.txt")
+	objects := direct(file.ReadLines("orbits.txt"))
 
-	fmt.Println("part one:", count(orbits))
-}
-func count(orbits []string) (total int) {
-	objects := direct(orbits)
-
-	for object := range objects {
-		total += len(transfers(object, "COM", objects))
-	}
-
-	return
+	fmt.Println("part one:", sum(objects))
+	fmt.Println("part two:", shortest(objects))
 }
 func direct(orbits []string) (objects map[string]string) {
 	objects = make(map[string]string)
@@ -31,10 +23,35 @@ func direct(orbits []string) (objects map[string]string) {
 
 	return
 }
+func sum(objects map[string]string) (total int) {
+	for object := range objects {
+		total += len(transfers(object, "COM", objects))
+	}
+
+	return
+}
 func transfers(source string, target string, objects map[string]string) (transfers []string) {
 	for source != target {
 		transfers = append(transfers, objects[source])
 		source = objects[source]
 	}
 	return
+}
+func shortest(objects map[string]string) int {
+	you := transfers("YOU", "COM", objects)
+	san := transfers("SAN", "COM", objects)
+	intersect := intersect(you, san)
+
+	return len(transfers("YOU", intersect, objects)) +
+		len(transfers("SAN", intersect, objects)) - 2 // exclude steps to reach intersection
+}
+func intersect(you []string, san []string) string {
+	for _, self := range you {
+		for _, other := range san {
+			if self == other {
+				return self
+			}
+		}
+	}
+	return ""
 }
